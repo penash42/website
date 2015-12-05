940119381a7bed235f8103786f01ae8bdd126efb
penash42 <adorafestiva@gmail.com>
2015-12-05 07:55:42 -0600

    Attempting to add tooltips to the timeline

--- script/timeline.js
+++ script/timeline.js
@@ -1,5 +1,5 @@
-const COLORS  = ["#000000", "red", "blue", "green", "purple", "yellow", "orange", "pink"];
-const OPACITY = .8;
+const COLORS  = ["#00ff31", "#0ff", "#69039f", "#ff246f", "#f00", "#f8ff00", "#ff7300"];
+const OPACITY = .9;
 
 $( document ).ready(function() {
     var id = $(".timeline-base").attr("id");
@@ -25,7 +25,6 @@ $( document ).ready(function() {
     
     // Create the list elements for each year
     var baseInnerHtml = "";
-    //var months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
     var totalYears = 0;
     for (var year = delta[0]; year <= delta[1]; year++)
     {
@@ -83,13 +82,58 @@ $( document ).ready(function() {
                 rectHeight += 25;
             }
             
-            bottom = bottom - rectHeight - 1;
-            var rectangle = canvas.rect(startLoc, bottom, endLoc, rectHeight).attr({fill: COLORS[rectangles.length], opacity: OPACITY}).mouseover(function () {
-                rectangle.stop().animate({transform: "t-50, -50"}, 500, "elastic");
-            }).mouseout(function () {
-                p.stop().animate({transform: ""}, 500, "elastic");
-                txt.stop().animate({opacity: 0}, 500);
+            bottom = bottom - rectHeight + 1;
+            
+            var rectangle = canvas.rect(startLoc, bottom, endLoc, rectHeight).attr({
+                fill: COLORS[rectangles.length],
+                opacity: OPACITY,
             });
+            
+            var childDiv = $(this).children();
+            /*rectangle.mouseover(function () {
+                $(childDiv).css("display", "block");
+                rectangle.stop().animate({transform: "t0, -35", height: rectHeight + 45}, 1, "bounce");
+            }).mouseout(function () {
+                rectangle.stop().animate({transform: "", height: rectHeight + 1}, 300, "bounce");
+            });*/
+            
+            rectangle.mouseover(function () {
+                $("#myHoverContents").css('display','block'); //show the div
+                this.attr({
+                    cursor: 'pointer'
+                });
+                this.animate({
+                    scale: '1.5',
+                    height: '110%',
+                    easing: 'linear'
+                }, 200);
+            });
+            rectangle.mouseout(function () {
+                $("#myHoverContents").css('display','none'); //hide the div
+                this.animate({
+                    scale: '1.05'
+                }, 200);
+                this.attr({
+                });
+            });
+            
+            /*var attr = { fill: '#898989', stroke: '#FFFFFF', 'stroke-width': 2, 'stroke-linejoin': 'round' }
+            var label = canvas.popup(50, 50, "").hide();
+            var labelText = $(this).html();
+            rectangle.hover(function(){
+                var bbox = this.getBBox();
+                label.attr({text: labelText}).update(bbox.x, bbox.y + bbox.height/2, bbox.width).toFront().show();
+                }, function(){ 
+                label.hide();
+            });*/
+            
+            var childDiv = $(this).children();
+            rectangle.hover(function() {
+                
+            }, function() {
+               // $(childDiv).hide();
+            });
+            
             rectangles.push({
                 start: startLoc,
                 end: endLoc + startLoc,
@@ -123,5 +167,3 @@ function willCauseCollision(rectangles, begin, end, height)
     }
     return false;
 }
-
-// pop ups
